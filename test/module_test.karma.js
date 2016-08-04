03ebf43f935f3f22049987662af04e7186bf8a9c
FujiHaruka <fujiharuka@realglobe.jp>
2016-08-04 19:38:12 +0900

    Add browser tests with karma

---
+++ test/module_test.karma.js
@@ -0,0 +1,60 @@
+/**
+ * Test case for module.
+ * Runs with karma.
+ */
+'use strict'
+
+const Module = require('../shim/browser/module.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('module', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Module', () => co(function * () {
+    let module = new Module({
+      foo () {
+        return 'foo!'
+      }
+    })
+    assert.ok(module)
+    assert.equal(module.foo(), 'foo!')
+
+    let { $spec } = module
+    assert.ok($spec)
+    assert.ok($spec.methods.foo)
+  }))
+
+  it('Module with custom spec', () => co(function * () {
+    let module = new Module({
+      foo () {
+        return 'foo!'
+      },
+      $spec: {
+        name: 'foo',
+        version: '1.0.0',
+        methods: {
+          foo: {
+            name: 'foo',
+            params: [
+              { name: 'hoge' }
+            ]
+          }
+        }
+      }
+    })
+    let { $spec } = module
+    assert.ok(module.foo)
+    assert.ok($spec.methods.foo)
+  }))
+})
+
+/* global describe, before, after, it */
---
+++ test/validate_test.karma.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for validate.
+ * Runs with karma.
+ */
+'use strict'
+
+const validate = require('../shim/browser/configure/validate.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('validate', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Validate', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
